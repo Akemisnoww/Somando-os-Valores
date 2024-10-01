d737ce5dc33348bd94bfa1ee2293c34eb1befe8e
Akemi 3 <akemisnoww@gmail.com>
2024-10-01 14:38:39 -0300

    Add files via upload
    esse programa soma 3 valores diferentes

---
+++ Somando Valores.py
@@ -0,0 +1,8 @@
+numero1 = int(input('me fale um numero'))
+numero2 = int(input('me fale outro numero'))
+numero3 = int(input('me fale mais um numero'))
+
+resultado = (numero1 + numero2 + numero3)
+
+print(resultado)
+
